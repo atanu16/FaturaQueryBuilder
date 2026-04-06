c6252ccdcf326a281f852bdfd10f8f02bf351382
Atanu Bera <atanumax2@gmail.com>
2026-04-07 02:00:16 +0530

    N-Grid fix

--- obj/Release/net8.0-windows/win-x64/QueryBuilder.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/QueryBuilder.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueryBuilder")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7305368744680e8fbf0949d8746cf06d17a122ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueryBuilder")]
